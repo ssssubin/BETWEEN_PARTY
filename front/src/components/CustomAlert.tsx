47f4d41fc43104ecfd8beac0a63e06058381289d
ssssubin <cdw3521@gmail.com>
2026-09-05 16:01:03 +0900

    💄: 커스텀 알람 스타일 수정

--- front/src/components/CustomAlert.tsx
+++ front/src/components/CustomAlert.tsx
@@ -1,10 +1,20 @@
 import "./CustomAlert.css";
 
-export default function CustomAlert({ open, title, message, onClose }: { open: boolean; title: string; message: string; onClose: any }) {
+export default function CustomAlert({
+  open,
+  title,
+  message,
+  onClose,
+}: {
+  open: boolean;
+  title: string;
+  message: string;
+  onClose: () => void;
+}) {
   if (!open) return null;
 
   return (
-    <div className="alert-overlay">
+    <div className={`alert-overlay ${open ? "open" : ""}`}>
       <div className="alert-box">
         <h2>{title}</h2>
         <p>{message}</p>
